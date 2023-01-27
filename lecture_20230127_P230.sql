7c773c6b8f0ce9bfaa270a84a165c4d9bf107683
Seungju Shin <shinsuengju1@gmail.com>
2023-01-27 15:49:41 +0900

    second commit

--- lecture_20230127_P230.sql
+++
@@ -1,9 +0,0 @@
--- 실습
-SELECT userid AS '사용자', SUM(price*amount) AS '총구매액'
-FROM buyTBL
-GROUP BY userid;
-
-WITH abc(userid, total)
-AS
-(SELECT userid AS '사용자', SUM(price*amount) FROM buyTBL GROUP BY userid)
-SELECT * FROM abc ORDER BY total DESC;
